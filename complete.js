007b3a325b02d0aef0705442ac8c4bcad3bf8358
Benjamin Carleski <Benjamin.Carleski@ATImetals.com>
2017-10-09 19:25:38 -0700

    Updates to complete.js

--- complete.js
+++ complete.js
@@ -4,7 +4,7 @@ var postMap = {};
 var comments = null;
 var postListDiv = null;
 
-function RequestPage(relativeUrl, content, success, error) {
+function requestPage(relativeUrl, content, success, error) {
     if (typeof(relativeUrl) !== 'string') return;
     if (typeof(content) === 'function') { error = success; success = content; content = null; }
     if (typeof(content) !== 'undefined' && content !== null && typeof(content) !== 'string') content = JSON.stringify(content);
@@ -33,7 +33,7 @@ function RequestPage(relativeUrl, content, success, error) {
     }
 }
 
-function LoadPosts(data) {
+function loadPosts(data) {
     console.log('Got Posts');
     posts = data || [];
 
@@ -43,10 +43,10 @@ function LoadPosts(data) {
         posts[i] = post;
     }
 
-    if (comments !== null) ShowPostsAndComments();
+    if (comments !== null) showPostsAndComments();
 }
 
-function LoadComments(data) {
+function loadComments(data) {
     console.log('Got Comments');
     comments = data || [];
 
@@ -54,10 +54,10 @@ function LoadComments(data) {
         comments[i] = new Comment(comments[i]);
     }
 
-    if (posts !== null) ShowPostsAndComments();
+    if (posts !== null) showPostsAndComments();
 }
 
-function ShowPostsAndComments() {
+function showPostsAndComments() {
     console.log('Showing Posts and Comments');
     for (var i = 0; i < comments.length; i++) {
         var comment = comments[i];
@@ -69,96 +69,143 @@ function ShowPostsAndComments() {
         }
     }
 
-    var html = [];
+    postListDiv.innerHTML = '';
     for (var i = 0; i < posts.length; i++) {
-        AddPost(html, posts[i]);
+		postListDiv.appendChild(posts[i].getPostElement());
     }
 
-    postListDiv.innerHTML = html.join('');
+	var allComments = document.getElementsByClassName('comment');
+
+	for (var j = 0; allComments && j < allComments.length; j++) {
+		var comment = allComments[j];
+		var commentId = comment.id;
+		var name = comment.getElementsByClassName('comment-name');
+		name = name && name.length ? name[0] : null;
+		var email = name ? name.getElementsByClassName('comment-email') : null;
+		email = email && email.length ? email[0] : null
+
+		// Simulate finding all comments for the current user, so that they can delete their own comments
+		if (email && /n@/.test(email.innerHTML)) {
+			var btn = document.createElement('button');
+			btn.innerHTML = 'X';
+			btn.setAttribute('title', 'Delete Comment');
+			btn.setAttribute('data-comment-id', commentId);
+			btn.addEventListener('click', removeComment);
+			name.insertBefore(btn, name.childNodes[0]);
+		}
+	}
 }
 
-function AddPost(html, post) {
-    if (!post) return;
-
-    html.push('<div class="post" id="post');
-    html.push(post.id);
-    html.push('"><div class="post-title">');
-    html.push(post.title);
-    html.push('</div><div class="post-body">');
-    html.push(post.body);
-    html.push('</div>');
-
-    AddUser(html, post.user);
-    
-    if (post.comments && post.comments.length) {
-        html.push('<div class="comments-list">Comments:');
-
-        for (var j = 0; j < post.comments.length; j++) {
-            AddComment(html, post.comments[j]);
-        }
-
-        html.push('</div>');
-    }
-
-    html.push('</div>');
+function removeComment() {
+	var commentId = this.getAttribute('data-comment-id');
+	var comment = commentId ? document.getElementById(commentId) : null;
+	
+	if (comment) comment = comment.parentNode.removeChild(comment);
 }
 
-function AddComment(html, comment) {
-    if (!comment) return;
-    
-    html.push('<div class="comment" id="comment');
-    html.push(comment.id);
-    html.push('"><div class="comment-body">');
-    html.push(comment.body);
-    html.push('</div><div class="comment-name">By ');
-    html.push(comment.name);
-    if (comment.email) {
-        html.push(' (<a class="comment-email" href="mailto:');
-        html.push(comment.email);
-        html.push('">');
-        html.push(comment.email);
-        html.push('</a>)');
-    }
-    html.push('</div></div>');
-}
+function TitledBody(id, title, body, className) {
+	var me = this;
+	me.id = id || 0;
+	me.title = title || 'A Title';
+	me.body = body || '';
+	me.className = className || 'element';
 
-function AddUser(html, user) {
-    if (!user) return;
-    
+	me.getElement = function (includeBody) {
+		var elem = document.createElement('div');
+		elem.className = me.className;
+		elem.id = me.className + me.id;
+		
+		var ttl = document.createElement('div');
+		ttl.className = me.className + '-title';
+		ttl.textContent = typeof(me.title) === 'function' ? me.title() : me.title;
+		elem.appendChild(ttl);
+		
+		if (includeBody) elem.appendChild(me.getBodyElement());
+		
+		return elem;
+	};
+	me.getBodyElement = function () {
+		var bdy = document.createElement('div');
+		bdy.className = me.className + '-body';
+		bdy.textContent = typeof(me.body) === 'function' ? me.body() : me.body;
+		return bdy;
+	};
 }
 
 function Post(rawPost) {
     var me = this;
 
-    me.id = rawPost.id || 0;
+	rawPost = rawPost || {};
+	TitledBody.call(me, rawPost.id, rawPost.title, rawPost.body, 'post');
+
     me.userId = rawPost.userId || 0;
-    me.title = rawPost.title || 'A Title';
-    me.body = rawPost.body || '';
 
     me.comments = [];
     me.user = null;
+
+	me.getPostElement = function () {
+		var elem = me.getElement(true);
+
+		if (me.comments && me.comments.length) {
+			var lst = document.createElement('div');
+			lst.className = 'comments-list';
+
+			for (var i = 0; i < me.comments.length; i++) {
+				lst.appendChild(me.comments[i].getCommentElement());
+			}
+			
+			elem.appendChild(lst);
+		}
+		
+		return elem;
+	};
 }
+Post.prototype = Object.create(TitledBody.prototype);
+Post.prototype.constructor = Post;
+
 function Comment(rawComment) {
     var me = this;
 
-    me.id = rawComment.id || 0;
+	rawComment = rawComment || {};
+	TitledBody.call(me, rawComment.id, rawComment.name || function() { return 'RE: ' + (me.post ? me.post.title : ''); } , rawComment.body, 'comment');
+
     me.postId = rawComment.postId || 0;
-    me.name = rawComment.name || 'Commenter';
     me.email = rawComment.email || null;
-    me.body = rawComment.body || '';
 
     me.post = null;
-}
 
-function ShowError(error) {
+	me.getCommentElement = function () {
+		var elem = me.getElement(true);
+		var name = document.createElement('div');
+		name.className = 'comment-name';
+		name.innerHTML = 'By ';
+		
+		var email = document.createElement('a');
+		email.className = 'comment-email';
+		email.href = 'mailto:' + me.email;
+		email.textContent = me.email;
+		name.appendChild(email);
+		
+		elem.appendChild(name);
+		
+		return elem;
+	};
+}
+Comment.prototype = Object.create(TitledBody.prototype);
+Comment.prototype.constructor = Comment;
+
+function showError(error) {
     postListDiv.innerHTML = 'Could not load the posts - ' + error;
 }
 
-function HandleContentLoaded() {
+function handleContentLoaded() {
     console.log('ContentLoaded');
     postListDiv = document.getElementById('postList');
-    RequestPage('/posts', LoadPosts);
-    RequestPage('/comments', LoadComments);
+	//Simulate server processing time in loading a complicated pageX
+	setTimeout(function() {
+		requestPage('/posts', loadPosts);
+		requestPage('/comments', loadComments);
+	}, 4000);
 }
 
-document.addEventListener("DOMContentLoaded", HandleContentLoaded);
+document.addEventListener("DOMContentLoaded", handleContentLoaded);
